8d567006c000d49065797322bfc9f197f53a1524
Dev Service User <devser.user@ipsosgroup.ipsos.com>
2019-09-03 13:20:06 +0300

    SkobeApi added

---
+++ SkobeApi/SkobeApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SkobeApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiWithAction",
+                routeTemplate: "{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ExportCell",
+                routeTemplate: "{controller}/{action}"
+            );
+
+            config.EnableCors();
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+
+        }
+    }
+}
---
+++ SkobeApi/SkobeApi/Controllers/ServicesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Dapper;
+
+namespace SkobeApi.Controllers
+{
+    public class ServicesController : ApiController
+    {
+        IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString);
+
+        public class OilSampling
+        {
+            public string CustomerName { get; set; }
+            public string SubstationName { get; set; }
+            public string Country { get; set; }
+            public string SamplingDate { get; set; }
+            public string CompanyName { get; set; }
+            public string ContactPersonName { get; set; }
+            public string Phone { get; set; }
+            public string Email { get; set; }
+            public string CompanyAddress { get; set; }
+            public string SampleNr { get; set; }
+            public string OilTemp { get; set; }
+            public string AmbientTemp { get; set; }
+            public string OilName { get; set; }
+            public string Language { get; set; }
+            public string LanguageOther { get; set; }
+            public string OilReportStandard { get; set; }
+            public string PlaceOfSampling { get; set; }
+            public string Rating { get; set; }
+            public string PrimaryVoltage { get; set; }
+            public string SecondaryVoltage { get; set; }
+            public string Frequency { get; set; }
+            public string SerialNumber { get; set; }
+            public string YearOfManufacture { get; set; }
+            public string OffLoad { get; set; }
+            public string OnLoad { get; set; }
+            public string OilAge { get; set; }
+            public string CoolingType { get; set; }
+            public string OilType { get; set; }
+            public string EquipmentType { get; set; }
+        }
+
+        [HttpGet]
+        [ActionName("Test")]
+        public IHttpActionResult Test()
+        {
+            return Ok("Test Ok");
+        }
+
+        [HttpPost]
+        [ActionName("OilSamplingSave")]
+        public IHttpActionResult OilSamplingSave(OilSampling os)
+        {
+            if (os != null)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("\n INSERT INTO [Services-Aydas-OilSampling] (RecordDate, CustomerName, SubstationName, Country, SamplingDate, CompanyName, ContactPersonName, Phone, Email, CompanyAddress, SampleNr, OilTemp, AmbientTemp, OilName, Language, LanguageOther, OilReportStandard, PlaceOfSampling, Rating, PrimaryVoltage, SecondaryVoltage, Frequency, SerialNumber, YearOfManufacture, OffLoad, OnLoad, OilAge, CoolingType, OilType, EquipmentType) ");
+                sb.Append("\n VALUES (GETDATE(),'"+ Clean(os.CustomerName) + "', '"+Clean(os.SubstationName) +"', '"+ Clean(os.Country) +"', '"+Clean(os.SamplingDate) +"', '"+Clean(os.CompanyName) +"', '"+Clean(os.ContactPersonName) +"', '"+ Clean(os.Phone) +"', '"+Clean(os.Email) +"', '"+Clean(os.CompanyAddress) +"', '"+ Clean(os.SampleNr) +"', '"+ Clean(os.OilTemp) +"', '"+Clean(os.AmbientTemp) +"', '"+ Clean(os.OilName) +"', '"+ Clean(os.Language) +"', '"+ Clean(os.LanguageOther) +"', '"+ Clean(os.OilReportStandard) +"', '"+ Clean(os.PlaceOfSampling) +"', '"+ Clean(os.Rating) +"', '"+ Clean(os.PrimaryVoltage) + "', '"+ Clean(os.SecondaryVoltage) +"', '"+ Clean(os.Frequency) +"', '"+ Clean(os.SerialNumber) +"', '"+ Clean(os.YearOfManufacture) +"', '"+ Clean(os.OffLoad) +"', '"+ Clean(os.OnLoad) +"', '"+ Clean(os.OilAge) +"', '"+ Clean(os.CoolingType) +"', '"+Clean(os.OilType) +"', '"+ Clean(os.EquipmentType) +"')");
+                db.Execute(sb.ToString());
+                return Ok("OK");
+            }
+            else
+            {
+                return Ok(0);
+            }
+        }
+
+
+        public string Clean(string StringToClean)
+        {
+            if (StringToClean != null)
+            {
+                StringToClean = StringToClean.Replace("'", "´");
+                StringToClean = StringToClean.Replace("<", "");
+                StringToClean = StringToClean.Replace(">", "");
+                StringToClean = StringToClean.Trim();
+            }
+            else
+                StringToClean = "";
+            return StringToClean;
+        }
+
+    }
+}
