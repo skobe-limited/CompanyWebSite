38d500de455dae73be86cc3897ddd8e688d8e824
Dev Service User <devser.user@ipsosgroup.ipsos.com>
2020-03-11 22:35:44 +0300

    SEO analysis

--- skobe.co.uk/projects/bptl.co.uk/js/main.js
+++ skobe.co.uk/projects/bptl.co.uk/js/main.js
@@ -48,7 +48,7 @@
 		var wScroll = $(this).scrollTop();
 
 		// Fixed nav
-		wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
+		//wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
 
 		// Back To Top Appear
 		wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
